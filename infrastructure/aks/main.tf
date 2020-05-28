e5700e2ebfdc65526d03ec27b74eb4a9bc54c2bd
Pat Emery <pemery@jhancock.com>
2020-05-28 10:05:54 -0400

    Added azure creds to tf provider

--- infrastructure/aks/main.tf
+++ infrastructure/aks/main.tf
@@ -3,8 +3,13 @@ terraform {
 }
 
 provider "azurerm" {
-  tenant_id = var.tenant_id
-  version   = "~>1.43.0"
+  version         = "=2.4.0"
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+
+  features {}
 }
 
 # Resource Group
